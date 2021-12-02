e88bc5ffe424bf939faf04f9d9de7d19d1f91d13
Ben Noordhuis <info@bnoordhuis.nl>
2021-12-02 09:49:26 +0100

    factor out platform code

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@ pub struct Interface {
     address: IpAddr,
     scope_id: Option<u32>,
     netmask: IpAddr,
-    broadcast: Option<IpAddr>,
 }
 
 impl Interface {
@@ -68,30 +67,13 @@ impl Interface {
     pub fn netmask(&self) -> &IpAddr {
         &self.netmask
     }
-
-    pub fn broadcast(&self) -> Option<&IpAddr> {
-        match self.is_broadcast() {
-            true => self.broadcast.as_ref(),
-            false => None,
-        }
-    }
-
-    pub fn dstaddr(&self) -> Option<&IpAddr> {
-        match self.is_point_to_point() {
-            true => self.broadcast.as_ref(),
-            false => None,
-        }
-    }
 }
 
-#[cfg(target_os = "macos")]
-pub use macos::*;
+#[cfg(not(target_os = "windows"))]
+pub use unix::*;
 
-#[cfg(target_os = "linux")]
-pub use linux::*;
-
-#[cfg(target_os = "linux")]
-mod linux {
+#[cfg(not(target_os = "windows"))]
+mod unix {
     use super::Interface;
     use libc as c;
     use std::ffi::CStr;
@@ -101,6 +83,12 @@ mod linux {
     use std::net::IpAddr;
     use std::ptr;
     use std::ptr::NonNull;
+
+    #[cfg(target_os = "linux")]
+    use crate::linux::*;
+
+    #[cfg(target_os = "macos")]
+    use crate::macos::*;
 
     pub fn all() -> io::Result<All> {
         let mut base = ptr::null_mut();
@@ -148,176 +136,12 @@ mod linux {
         }
     }
 
-    fn is_packet(addr: *const c::sockaddr) -> bool {
-        Some(c::AF_PACKET) == family(addr)
-    }
-
     fn family(addr: *const c::sockaddr) -> Option<i32> {
         let not_null = !addr.is_null();
         not_null.then(|| unsafe { (*addr).sa_family } as i32)
     }
 
-    fn ipaddr(addr: *const c::sockaddr) -> Option<IpAddr> {
-        match family(addr) {
-            Some(c::AF_INET) => {
-                let addr = addr as *const c::sockaddr_in;
-                let addr = unsafe { (*addr).sin_addr.s_addr }.to_be_bytes();
-                let addr = net::Ipv4Addr::from(addr);
-                Some(IpAddr::V4(addr))
-            }
-            Some(c::AF_INET6) => {
-                let addr = addr as *const c::sockaddr_in6;
-                let addr = unsafe { (*addr).sin6_addr.s6_addr };
-                let addr = net::Ipv6Addr::from(addr);
-                Some(IpAddr::V6(addr))
-            }
-            _ => None,
-        }
-    }
-
-    fn to_interface(
-        base: Option<NonNull<c::ifaddrs>>,
-        curr: NonNull<c::ifaddrs>,
-    ) -> Option<Interface> {
-        let curr = unsafe { curr.as_ref() };
-        let addr = NonNull::new(curr.ifa_addr).unwrap();
-        let addr = unsafe { addr.as_ref() };
-
-        if is_packet(addr) {
-            return None;
-        }
-
-        let name = unsafe { CStr::from_ptr(curr.ifa_name) };
-
-        let mac = Iter(base)
-            .find_map(|link| mac_of(name, link))
-            .unwrap_or_default();
-
-        let name = name.to_string_lossy().into_owned();
-        let flags = curr.ifa_flags;
-        let netmask = ipaddr(curr.ifa_netmask).unwrap_or_else(no_addr);
-        let broadcast = ipaddr(curr.ifa_ifu);
-
-        let scope_id = (addr.sa_family == c::AF_INET6 as _).then(|| {
-            let addr = addr as *const _ as *const c::sockaddr_in6;
-            unsafe { (*addr).sin6_scope_id }
-        });
-
-        let address = ipaddr(addr).unwrap_or_else(no_addr);
-
-        Some(Interface {
-            name,
-            flags,
-            mac,
-            address,
-            scope_id,
-            netmask,
-            broadcast,
-        })
-    }
-
-    fn mac_of(name: &CStr, link: NonNull<c::ifaddrs>) -> Option<[u8; 6]> {
-        let link = unsafe { link.as_ref() };
-
-        if !is_packet(link.ifa_addr) {
-            return None;
-        }
-
-        let ok = unsafe { CStr::from_ptr(link.ifa_name) }
-            .to_bytes()
-            .strip_prefix(name.to_bytes())
-            .filter(|suffix| suffix.is_empty() || suffix.starts_with(b":"))
-            .is_some();
-
-        if !ok {
-            return None;
-        }
-
-        let addr = link.ifa_addr as *const _ as *const c::sockaddr_ll;
-        let addr = unsafe { &*addr };
-
-        if addr.sll_halen != 6 {
-            return None;
-        }
-
-        let [b0, b1, b2, b3, b4, b5, _, _] = addr.sll_addr;
-
-        Some([b0, b1, b2, b3, b4, b5])
-    }
-
-    fn no_addr() -> IpAddr {
-        IpAddr::V4(net::Ipv4Addr::UNSPECIFIED)
-    }
-}
-
-#[cfg(target_os = "macos")]
-mod macos {
-    use super::Interface;
-    use libc as c;
-    use std::ffi::CStr;
-    use std::io;
-    use std::mem;
-    use std::net;
-    use std::net::IpAddr;
-    use std::ptr;
-    use std::ptr::NonNull;
-
-    pub fn all() -> io::Result<All> {
-        let mut base = ptr::null_mut();
-
-        if 0 != unsafe { c::getifaddrs(&mut base) } {
-            return Err(io::Error::last_os_error());
-        }
-
-        let base = NonNull::new(base);
-        let iter = Iter(base);
-
-        Ok(All { base, iter })
-    }
-
-    pub struct All {
-        base: Option<NonNull<c::ifaddrs>>,
-        iter: Iter,
-    }
-
-    impl Iterator for All {
-        type Item = Interface;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            self.iter.find_map(|curr| to_interface(self.base, curr))
-        }
-    }
-
-    impl Drop for All {
-        fn drop(&mut self) {
-            if let Some(mut base) = self.base {
-                unsafe { c::freeifaddrs(base.as_mut()) };
-            }
-        }
-    }
-
-    struct Iter(Option<NonNull<c::ifaddrs>>);
-
-    impl Iterator for Iter {
-        type Item = NonNull<c::ifaddrs>;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            let curr = self.0?;
-            let next = unsafe { curr.as_ref().ifa_next };
-            mem::replace(&mut self.0, NonNull::new(next))
-        }
-    }
-
-    fn is_link(addr: *const c::sockaddr) -> bool {
-        Some(c::AF_LINK) == family(addr)
-    }
-
-    fn family(addr: *const c::sockaddr) -> Option<i32> {
-        let not_null = !addr.is_null();
-        not_null.then(|| unsafe { (*addr).sa_family } as i32)
-    }
-
-    fn ipaddr(addr: *const c::sockaddr) -> Option<IpAddr> {
+    fn ip(addr: *const c::sockaddr) -> Option<IpAddr> {
         match family(addr) {
             Some(c::AF_INET) => {
                 let addr = addr as *const c::sockaddr_in;
@@ -355,15 +179,14 @@ mod macos {
 
         let name = name.to_string_lossy().into_owned();
         let flags = curr.ifa_flags;
-        let netmask = ipaddr(curr.ifa_netmask).unwrap_or_else(no_addr);
-        let broadcast = ipaddr(curr.ifa_dstaddr);
+        let netmask = ip(curr.ifa_netmask).unwrap_or_else(no_addr);
 
-        let scope_id = (addr.sa_family == c::AF_INET6 as u8).then(|| {
+        let scope_id = (addr.sa_family == c::AF_INET6 as _).then(|| {
             let addr = addr as *const _ as *const c::sockaddr_in6;
             unsafe { (*addr).sin6_scope_id }
         });
 
-        let address = ipaddr(addr).unwrap_or_else(no_addr);
+        let address = ip(addr).unwrap_or_else(no_addr);
 
         Some(Interface {
             name,
@@ -372,8 +195,75 @@ mod macos {
             address,
             scope_id,
             netmask,
-            broadcast,
         })
+    }
+
+    fn no_addr() -> IpAddr {
+        IpAddr::V4(net::Ipv4Addr::UNSPECIFIED)
+    }
+}
+
+#[cfg(target_os = "linux")]
+mod linux {
+    use libc as c;
+    use std::ffi::CStr;
+    use std::ptr::NonNull;
+
+    pub(crate) fn is_link(addr: *const c::sockaddr) -> bool {
+        Some(c::AF_PACKET) == family(addr)
+    }
+
+    fn family(addr: *const c::sockaddr) -> Option<i32> {
+        let not_null = !addr.is_null();
+        not_null.then(|| unsafe { (*addr).sa_family } as i32)
+    }
+
+    pub(crate) fn mac_of(
+        name: &CStr,
+        link: NonNull<c::ifaddrs>,
+    ) -> Option<[u8; 6]> {
+        let link = unsafe { link.as_ref() };
+
+        if !is_link(link.ifa_addr) {
+            return None;
+        }
+
+        let ok = unsafe { CStr::from_ptr(link.ifa_name) }
+            .to_bytes()
+            .strip_prefix(name.to_bytes())
+            .filter(|suffix| suffix.is_empty() || suffix.starts_with(b":"))
+            .is_some();
+
+        if !ok {
+            return None;
+        }
+
+        let addr = link.ifa_addr as *const _ as *const c::sockaddr_ll;
+        let addr = unsafe { &*addr };
+
+        if addr.sll_halen != 6 {
+            return None;
+        }
+
+        let [b0, b1, b2, b3, b4, b5, _, _] = addr.sll_addr;
+
+        Some([b0, b1, b2, b3, b4, b5])
+    }
+}
+
+#[cfg(target_os = "macos")]
+mod macos {
+    use libc as c;
+    use std::ffi::CStr;
+    use std::ptr::NonNull;
+
+    fn is_link(addr: *const c::sockaddr) -> bool {
+        Some(c::AF_LINK) == family(addr)
+    }
+
+    fn family(addr: *const c::sockaddr) -> Option<i32> {
+        let not_null = !addr.is_null();
+        not_null.then(|| unsafe { (*addr).sa_family } as i32)
     }
 
     fn mac_of(name: &CStr, link: NonNull<c::ifaddrs>) -> Option<[u8; 6]> {
@@ -403,10 +293,6 @@ mod macos {
         let [b0, b1, b2, b3, b4, b5, _, _, _, _, _, _] = addr.sdl_data;
 
         Some([b0 as u8, b1 as u8, b2 as u8, b3 as u8, b4 as u8, b5 as u8])
-    }
-
-    fn no_addr() -> IpAddr {
-        IpAddr::V4(net::Ipv4Addr::UNSPECIFIED)
     }
 }
 
